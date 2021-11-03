f69f896eb8d21451cf492c68c0319e84fbb83ee9
K-Daniel98 <kantordani24@gmail.com>
2021-11-03 23:02:45 +0100

    Removed BookedSeat

--- ticket-service/src/main/java/com/epam/training/ticketservice/core/booking/repository/SeatRepository.java
+++
@@ -1,7 +0,0 @@
-package com.epam.training.ticketservice.core.booking.repository;
-
-import com.epam.training.ticketservice.core.booking.model.BookedSeat;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SeatRepository extends JpaRepository<BookedSeat, Long> {
-}
