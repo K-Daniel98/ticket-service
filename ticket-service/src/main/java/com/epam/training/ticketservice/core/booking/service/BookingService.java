fe18b351c981c987e4776bb117badd18371e0152
K-Daniel98 <kantordani24@gmail.com>
2021-11-03 23:07:28 +0100

    Booking tickets should return a set of bookings

--- ticket-service/src/main/java/com/epam/training/ticketservice/core/booking/service/BookingService.java
+++ ticket-service/src/main/java/com/epam/training/ticketservice/core/booking/service/BookingService.java
@@ -2,7 +2,10 @@ package com.epam.training.ticketservice.core.booking.service;
 
 import com.epam.training.ticketservice.core.booking.model.Booking;
 import com.epam.training.ticketservice.core.screening.model.Screening;
+import com.epam.training.ticketservice.core.user.model.User;
+
+import java.util.Set;
 
 public interface BookingService {
-    Booking book(String movieName, String roomName, String screeningTime, String seats);
+    Set<Booking> book(User user, String movieName, String roomName, String screeningTime, String seats);
 }
