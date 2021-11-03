7227c7f54b9f0a2b175a95ebf164ee7032ebdc2b
K-Daniel98 <kantordani24@gmail.com>
2021-11-03 23:07:40 +0100

    Updated BookingServiceImpl

--- ticket-service/src/main/java/com/epam/training/ticketservice/core/booking/service/impl/BookingServiceImpl.java
+++ ticket-service/src/main/java/com/epam/training/ticketservice/core/booking/service/impl/BookingServiceImpl.java
@@ -1,9 +1,11 @@
 package com.epam.training.ticketservice.core.booking.service.impl;
 
+import com.epam.training.ticketservice.core.booking.exception.SeatAlreadyTakenException;
 import com.epam.training.ticketservice.core.booking.exception.SeatDoesNotExistException;
 import com.epam.training.ticketservice.core.booking.model.BookedSeat;
 import com.epam.training.ticketservice.core.booking.model.Booking;
 import com.epam.training.ticketservice.core.booking.repository.BookingRepository;
+import com.epam.training.ticketservice.core.booking.repository.SeatRepository;
 import com.epam.training.ticketservice.core.booking.service.BookingService;
 import com.epam.training.ticketservice.core.movie.exception.MovieDoesNotExistException;
 import com.epam.training.ticketservice.core.movie.service.MovieService;
@@ -14,17 +16,23 @@ import com.epam.training.ticketservice.core.screening.exception.ScreeningDoesNot
 import com.epam.training.ticketservice.core.screening.model.Screening;
 import com.epam.training.ticketservice.core.screening.repository.ScreeningRepository;
 import com.epam.training.ticketservice.core.screening.service.ScreeningService;
+import com.epam.training.ticketservice.core.user.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.awt.print.Book;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Service
 public class BookingServiceImpl implements BookingService {
 
     private final BookingRepository bookingRepository;
+    private final SeatRepository seatRepository;
+    private final ScreeningRepository screeningRepository;
     private final ScreeningService screeningService;
     private final RoomService roomService;
     private final MovieService movieService;
@@ -33,16 +41,20 @@ public class BookingServiceImpl implements BookingService {
     private static final long basePrice = 1500;
 
     @Autowired
-    public BookingServiceImpl(BookingRepository bookingRepository, ScreeningService screeningService, RoomService roomService, MovieService movieService, DateTimeFormatter formatter) {
+    public BookingServiceImpl(ScreeningRepository screeningRepository, BookingRepository bookingRepository,
+                              ScreeningService screeningService, RoomService roomService, MovieService movieService,
+                              DateTimeFormatter formatter, SeatRepository seatRepository) {
+        this.screeningRepository = screeningRepository;
         this.bookingRepository = bookingRepository;
         this.screeningService = screeningService;
         this.roomService = roomService;
         this.movieService = movieService;
         this.formatter = formatter;
+        this.seatRepository = seatRepository;
     }
 
     @Override
-    public Booking book(String movieName, String roomName, String screeningTime, String seats) {
+    public Set<Booking> book(User user, String movieName, String roomName, String screeningTime, String seats) {
 
         var movie = movieService.getMovieByName(movieName);
 
@@ -56,17 +68,19 @@ public class BookingServiceImpl implements BookingService {
             throw new RoomDoesNotExistException(roomName);
         }
 
-        var screening = screeningService.getScreeningByMovieAndRoomAndScreeningTime(
+        var plannedScreening = screeningService.getScreeningByMovieAndRoomAndScreeningTime(
             movie.get(),
             room.get(),
             LocalDateTime.parse(screeningTime, formatter)
         );
 
-        if (screening.isEmpty()) {
+        if (plannedScreening.isEmpty()) {
             throw new ScreeningDoesNotExistException();
         }
 
-        var bookedSeats = new HashSet<BookedSeat>();
+        var screening = plannedScreening.get();
+
+        var finalPrice = basePrice * seats.length();
 
         var splitted = seats.split(" ");
 
@@ -74,28 +88,27 @@ public class BookingServiceImpl implements BookingService {
 
             var seatSplitted = seatData.split(",");
 
-            var seat = new BookedSeat(
-                Integer.parseInt(seatSplitted[0]),
-                Integer.parseInt(seatSplitted[1])
-            );
+            var row = Integer.parseInt(seatSplitted[0]);
+            var column = Integer.parseInt(seatSplitted[1]);
 
-            if (!doesSeatExist(room.get(), seat.getRowNumber(), seat.getColumnNumber())) {
-                throw new SeatDoesNotExistException(seat);
+            var booking = new Booking(screening, user, row, column, finalPrice);
+
+            if (!doesSeatExist(room.get(), row, column)) {
+                throw new SeatDoesNotExistException(booking);
             }
 
-            if (bookingRepository.existsBookingBySeats(seat) || !bookedSeats.add(seat)) {
-                throw new SeatDoesNotExistException(seat);
+            if (isSeatTaken(screening.getBookings(), row, column)) {
+                throw new SeatAlreadyTakenException(booking);
             }
+
+            user.getBookings().add(booking);
+            screening.getBookings().add(booking);
 
         }
 
-        var finalPrice = basePrice * seats.length();
+        screeningRepository.save(screening);
 
-        var booking = new Booking(screening.get(), bookedSeats, finalPrice);
-
-        bookingRepository.save(booking);
-
-        return booking;
+        return screening.getBookings();
     }
 
     private static boolean doesSeatExist(Room room, int row, int column) {
@@ -104,5 +117,10 @@ public class BookingServiceImpl implements BookingService {
         }
 
         return column <= room.getColumns() && column >= 0;
+    }
+
+    private static boolean isSeatTaken(Set<Booking> bookings, int row, int column) {
+        return bookings.stream()
+            .anyMatch(booking -> booking.getRowNumber() == row && booking.getColumnNumber() == column);
     }
 }
