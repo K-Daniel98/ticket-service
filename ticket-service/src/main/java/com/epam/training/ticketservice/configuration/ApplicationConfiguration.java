78c4a9f947fc7d515d926062913503581c39e5b8
K-Daniel98 <kantordani24@gmail.com>
2021-11-04 11:02:11 +0100

    Added BasePrice bean

--- ticket-service/src/main/java/com/epam/training/ticketservice/configuration/ApplicationConfiguration.java
+++ ticket-service/src/main/java/com/epam/training/ticketservice/configuration/ApplicationConfiguration.java
@@ -1,12 +1,12 @@
 package com.epam.training.ticketservice.configuration;
 
+import com.epam.training.ticketservice.core.pricing.model.BasePrice;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import java.io.PrintStream;
 import java.time.format.DateTimeFormatter;
 
 @Configuration
@@ -23,8 +23,8 @@ public class ApplicationConfiguration {
     }
 
     @Bean
-    public PrintStream outputStream() {
-        return System.out;
+    public BasePrice basePrice() {
+        return new BasePrice(1500);
     }
 
 }
