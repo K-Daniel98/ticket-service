6b594befecdba95639c9985c0d1cdb0876722632
K-Daniel98 <kantordani24@gmail.com>
2021-11-04 11:03:46 +0100

    Removed unnecessary method

--- ticket-service/src/main/java/com/epam/training/ticketservice/core/booking/repository/BookingRepository.java
+++ ticket-service/src/main/java/com/epam/training/ticketservice/core/booking/repository/BookingRepository.java
@@ -1,9 +1,7 @@
 package com.epam.training.ticketservice.core.booking.repository;
 
-import com.epam.training.ticketservice.core.booking.model.BookedSeat;
 import com.epam.training.ticketservice.core.booking.model.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookingRepository extends JpaRepository<Booking, Long> {
-    //boolean existsBookingBySeats(BookedSeat seat);
 }
