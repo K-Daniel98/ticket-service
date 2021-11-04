2ca85286fc370349ee34ca5285b218d97a587903
K-Daniel98 <kantordani24@gmail.com>
2021-11-04 23:10:25 +0100

    Increased timeout

--- ticket-service-acceptance-tests/src/test/java/com/training/epam/ticketservice/at/GenericCliProcessStepDefs.java
+++ ticket-service-acceptance-tests/src/test/java/com/training/epam/ticketservice/at/GenericCliProcessStepDefs.java
@@ -14,7 +14,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GenericCliProcessStepDefs {
 
-    private static final int OUTPUT_TIMEOUT = 25000;
+    private static final int OUTPUT_TIMEOUT = 45000;
 
     private ProcessUnderTest cliProcess;
 
